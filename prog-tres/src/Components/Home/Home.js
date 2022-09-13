2701b2fc56072f2ef8e30da6b3e09ae867bca62e
MarcosBordeu <mbordeu@udesa.edu.ar>
2022-09-12 21:12:17 -0300

    avances y correccion de buscador

--- prog-tres/src/Components/Home/Home.js
+++ prog-tres/src/Components/Home/Home.js
@@ -9,12 +9,13 @@ class Home extends Component {
         this.state = {
             props: props,
             peliculas: [],
-            peliculasIniciales: []
+            peliculasIniciales: [],
+            valor: ''
         };
     };
 
     componentDidMount() {
-        //BUscamos datos
+        //Buscamos datos
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=1845c94396255a256363182ed898e8fc&language=en-US&page=1')
             .then(res => res.json())
             .then(data => this.setState({
@@ -24,11 +25,29 @@ class Home extends Component {
             .catch(err => console.log(err))
     }
 
-    buscarPeliculas(peliculaBuscada) {
-        let peliculasFiltradas = this.state.peliculasIniciales.filter(pelicula => pelicula.title.toLowerCase().includes(peliculaBuscada.toLowerCase()));
-        this.setState({
-            peliculas: peliculasFiltradas,
-        })
+    // buscarPeliculas(peliculaBuscada) {
+    //     let peliculasFiltradas = this.state.peliculasIniciales.filter(pelicula => pelicula.title.toLowerCase().includes(peliculaBuscada.toLowerCase()));
+    //     this.setState({
+    //         peliculas: peliculasFiltradas,
+    //     })
+    // }
+
+    buscadorP(event) {
+        event.preventDefault()
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${this.state.valor}?api_key=1845c94396255a256363182ed898e8fc&language=en-US`)
+        .then(response => response.json())
+        .then(data => this.setState({
+            peliculasT: data.results
+        }))
+        .catch(error => console.log(error))
+        console.log(this.state.peliculas);
+
+    }
+
+    controlarCambios(event){
+        this.setState(
+            {valor: event.target.value}, 
+            ()=>console.log(event.target.value))
     }
 
 
@@ -37,7 +56,13 @@ class Home extends Component {
         return (
             <React.Fragment>
 
-                <Buscador buscarPeliculas={(peliculaBuscada) => this.buscarPeliculas(peliculaBuscada)} />
+                {/* <Buscador buscarPeliculas={(peliculaBuscada) => this.buscarPeliculas(peliculaBuscada)} /> */}
+
+                <form onSubmit={(event) => this.buscadorP(event)}>
+                    <input type="text" onChange={(event) => this.controlarCambios(event)} value={this.state.valor} />
+                    <input type="submit" value=">" />
+                </form>
+                
 
                 <h2 className="title-home">Más populares</h2>
                 <section className='cardContainer'>
